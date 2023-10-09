bdd6811a8f88131bc49f594d0ca3c866d8d47ac3
tinawang98 <kikiwang9808@gmail.com>
2023-10-09 16:42:18 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,115 @@
+# print('I am Tina')
+#
+# #Variables
+# price = 10
+# rating = 4.9
+# name = 'Tina'
+# is_published = True
+# print(price)
+# patient_name = 'John Smith'
+# patient_age = 20
+# is_new_patient = True
+
+#Getting Input
+# name = input('What is your name? ')
+# fav_color = input('What is your favourite color? ')
+# print(name + ' likes ' + fav_color)
+
+#Type Conversion
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2023 - int(birth_year)
+# print(type(age))
+# print('My age is ' + str(age))
+#Exercise
+# weight_lbs = input('Your weight(lbs): ')
+# weight_kg = int(weight_lbs) * 0.45
+# print('Your weight in Kg is: ' + str(weight_kg))
+
+#String
+# course = 'Python for Beginners'
+# another = course[:]
+# print(course[0])
+# print(course[-2])
+# print(course[0:3])
+# print(course[:5])
+# print(another)
+# name = 'Jennifer'
+# print(name[1:-1])
+# print(name[1:7])
+
+#Formatted Strings
+# first = 'John'
+# last = 'Smith'
+# msg = f'{first} [{last}] is a coder'
+# print(msg)
+
+#String Methods
+# course = 'Python for Beginners'
+# print(len(course))
+# print(course.upper())
+# print(course)
+#
+# print(course.replace('n','A'))
+# print('python' in course)
+# print(course.title())
+
+#Arithmetic Operations
+# x = 10
+# x *= 3
+# print(x)
+
+#Operator Precedence
+# x = (10 + 3) * 2 ** 2
+# print(x)
+
+#Math Functions
+# import math
+# x = 2.9
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
+# # print(round(x))
+# # print(abs(-2.9))
+
+#If Statements
+# is_hot = False
+# is_cold = False
+#
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+# else:
+#     print("It's a lovely day")
+# print("Enjoy your day")
+#Exercise
+# price = 1000000
+# has_good_credit = True
+# if has_good_credit:
+#     down_payment = price * 0.1
+# else:
+#     down_payment = price * 0.2
+# print(f"Down payment: ${down_payment}")
+
+#Logical Operators
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
